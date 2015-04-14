2d0e936b9afa71c263409d064ef5c6dc6def4fde
Joseph Nouri <SingularTier@gmail.com>
2015-04-13 19:22:10 -0600

    Added Calculator class to allow AER to do calculations (from Win-Epic's fork)

---
+++ AerSpeech/AerLib/Calculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AerSpeech
+{
+    public class Calculator
+    {
+
+        private WebClient _dataDownloader;
+
+        public Calculator()
+        {
+            _dataDownloader = new WebClient();
+        }
+
+        public string CalculationResult(string query)
+        {
+            string result = null;
+
+            try
+            {
+                result = _dataDownloader.DownloadString("https://www.calcatraz.com/calculator/api?c=" + WebUtility.UrlEncode(query)).Trim();
+                if (result == "answer")
+                {
+                    result = null;
+                }
+            }
+            catch (Exception e)
+            {
+                AerDebug.LogException(e);
+            }
+            
+            return result;
+
+        }
+    }
+}
